a9e55906c9e78b412a2d056ae831a946168081bf
Charusmitha Deshpande <deshpande.c@husky.neu.edu>
2019-07-24 11:28:56 -0700

    Project

--- src/ErrorPage/ErrorPage.jsx
+++ src/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import styles from './ErrorPage.module.css';
 
 const ErrorPage = () => {
   return (
-    <div>
-      This is the 404 page
+    <div className={styles.error}>
+      <p className={styles.errorDesc}>404 PAGE NOT FOUND</p>
+      <p>The page you are looking for is unavailable.</p>
     </div>
   )
 }
